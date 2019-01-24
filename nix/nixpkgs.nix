c33dd654c878fda5f51b0179a4de396b4409570c
Chris Knox <chris.knox@nzherald.co.nz>
2019-01-24 13:09:21 +1300

    hmmm

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,7 +1,7 @@
 let
   src = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/7790dbe6a27d6bea22ecc62bf21841ccb427ec3b.tar.gz";
-    sha256 = "afc87397e0b56f9d7c232a9ef144af1bd832f8ea76c5495045be25f5171e8b58";
+    sha256 = "0qi9xssp2084amqp7pi0zwxvmygv74v9ywqd11fc9m86019wb8x4";
   };
 in
   import src
