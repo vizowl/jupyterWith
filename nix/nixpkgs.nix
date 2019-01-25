a93aba39546eb3c4ca2cdc4e93dc67dc2771df02
Chris Knox <chris.knox@nzherald.co.nz>
2019-01-25 13:38:48 +1300

    Use ghcpkgs version with matching sha

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   src = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/72eae722235bfba72f2d4bc29094209c9882fc5d.tar.gz";
+    url = "https://github.com/NixOS/nixpkgs/archive/7790dbe6a27d6bea22ecc62bf21841ccb427ec3b.tar.gz";
     sha256 = "0qi9xssp2084amqp7pi0zwxvmygv74v9ywqd11fc9m86019wb8x4";
   };
 in
